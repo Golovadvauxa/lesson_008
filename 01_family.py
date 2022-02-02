3596f77ea2f055089ac1781f0ce07cdabf196b27
Сергей <parenek.serega@gmail.com>
2022-02-02 13:13:14 +0300

    part 2 develop

--- 01_family.py
+++ 01_family.py
@@ -212,22 +212,22 @@ class Wife(Human):
 # Если кот дерет обои, то грязи становится больше на 5 пунктов
 
 
-# class Cat:
-#
-#     def __init__(self):
-#         pass
-#
-#     def act(self):
-#         pass
-#
-#     def eat(self):
-#         pass
-#
-#     def sleep(self):
-#         pass
-#
-#     def soil(self):
-#         pass
+class Cat:
+
+    def __init__(self):
+        pass
+
+    def act(self):
+        pass
+
+    def eat(self):
+        pass
+
+    def sleep(self):
+        pass
+
+    def soil(self):
+        pass
 
 
 ######################################################## Часть вторая бис
